ec46906616e08f43f8aecdca0dae3365efe4b970
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-07-05 13:12:59 -0300

    requisito 7 resolvido

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function adicionaPixel() {
   for (let index = 0; index < 25; index += 1) {
     const pixel = document.createElement('div');
     pixel.className = 'pixel';
+    pixel.addEventListener('click', () => {
+    }, false);
     const sectionPixelBoard = document.getElementById('pixel-board');
     sectionPixelBoard.appendChild(pixel);
   }
@@ -9,7 +11,17 @@ function adicionaPixel() {
 
 adicionaPixel();
 
-// window.onload = function () {
-//   const firstselected = document.querySelector('black');
-//   firstselected.className = 'selected';
-// };
+const colorBlack = document.getElementsByClassName('color')[0];
+colorBlack.classList.add('selected');
+
+function whenClick(event) {
+  const selected = document.getElementsByClassName('selected')[0];
+  selected.classList.remove('selected');
+  const changeSelected = event.target;
+  changeSelected.classList.add('selected');
+}
+
+const colors = document.getElementsByClassName('color');
+for (let index = 0; index < colors.length; index += 1) {
+  colors[index].addEventListener('click', whenClick);
+}
